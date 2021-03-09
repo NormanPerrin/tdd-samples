d35f945df22fd23c577585ead1fb95e19cd4f495
Norman Perrin <nperrin@riseup.net>
2021-03-09 22:53:28 +0100

    mutability: red

--- src/test.spec.ts
+++ src/test.spec.ts
@@ -5,4 +5,14 @@ describe('money exchange', () => {
         const five = new Dollar(5);
         expect(five.times(2).amount).toEqual(10);
     });
+
+    it('should not change the object', () => {
+        const five = new Dollar(5),
+            ten = five.times(2),
+            twenty = ten.times(2);
+
+        expect(five.amount).toEqual(5);
+        expect(ten.amount).toEqual(10);
+        expect(twenty.amount).toEqual(20);
+    });
 });
